dfc96457765c5036e5cfba70e93a298d68311cec
flow-zy <84438296+flow-zy@users.noreply.github.com>
2023-08-12 09:21:18 +0800

    chore: 修改config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'./',
+  server: {
+    port: 3000,
+    open: true
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname,'./src')
