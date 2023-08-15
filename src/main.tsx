b8b57c7c6701175ca7011b7cce647dfdf375c58b
flow-zy <84438296+flow-zy@users.noreply.github.com>
2023-08-15 10:55:11 +0800

    chore: 增加redux持久化缓存

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import ReactDOM from 'react-dom/client'
 import { Provider  } from 'react-redux'
 import App from './App.tsx'
-import { store } from './store'
+import { persistor, store } from './store'
+import {PersistGate } from 'redux-persist/integration/react'
 import './index.css'
 import 'reset.css'
 import '@/mock'
 import 'font-awesome/css/font-awesome.min.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Provider store={store}><App /></Provider>,
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+    </Provider>,
 
 )
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore,combineReducers } from '@reduxjs/toolkit'
+import {persistReducer,persistStore} from 'redux-persist'
 import userReducer from './slice/user'
-export const store = configureStore({
-  reducer: {
+import storage from 'redux-persist/es/storage'
+import thunk from 'redux-thunk'
+// persist配置
+const config={
+  key:'root',
+  storage,
+  blackList:[] // 不被缓存的
+}
+// 拆分reducer
+const reducer = combineReducers({
     user: userReducer,
-  },
 })
+// 
+const reducers=persistReducer(config,reducer)
+export const store = configureStore({
+  reducer:reducers,
+  middleware:[thunk ]
+})
+
+export const  persistor = persistStore(store)
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
