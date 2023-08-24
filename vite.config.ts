5b10535fff1ca2076b21d4608cc4dc2f57e7f67d
flow-zy <wz19121@yeah.net>
2023-08-24 13:20:27 +0800

    fix: 🐛 bug234

--- vite.config.ts
+++ vite.config.ts
@@ -80,16 +80,7 @@ export default defineConfig(({ mode }): UserConfig => {
 				output: {
 					chunkFileNames: 'assets/js/[name]-[hash].js',
 					entryFileNames: 'assets/js/[name]-[hash].js',
-					assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
-					manualChunks(id) {
-						if (id.includes('node_modules')) {
-							return id
-								.toString()
-								.split('node_modules/')[1]
-								.split('/')[0]
-								.toString()
-						}
-					}
+					assetFileNames: 'assets/[ext]/[name]-[hash].[ext]'
 				}
 			}
 		}
